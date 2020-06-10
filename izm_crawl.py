4a869a5ea5bc6f9b131aa3c9c6540a93a80f3b34
hanrimJO <whgksfla@gmail.com>
2020-06-10 17:25:18 +0900

    crawling: 평가목록 리스트 만들기

--- izm_crawl.py
+++ izm_crawl.py
@@ -12,6 +12,15 @@ headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleW
 url = "http://www.izm.co.kr/contentList.asp?bigcateidx=8&subcateidx=10&view_tp=1&view_sort=1&subjectchr=&periodidx=&page=1"
 response = requests.get(headers=headers, url=url)
 soup = BeautifulSoup(response.text, 'html.parser')
-kpop_table = soup.select('td', {'class': 'size13'})
-kpop_list = [(i.text).strip().replace('\n', '') for i in kpop_table]
-print(kpop_list)
+ul = soup.find('ul', id='summary1')
+contents = ul.select('table tr td table tr')
+contents_list = []
+for i in range(1, 31):
+    print(contents[i])
+    contents_list.append([])
+    contents_list[i-1].append(contents[i].text.strip().replace('\n', ' '))
+
+print(contents_list)
+# kpop_table = soup.select('td', {'class': 'size13'})
+# kpop_list = [(i.text).strip().replace('\n', '') for i in kpop_table]
+# print(kpop_list)
